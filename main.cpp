a8e339a91d0922ad7ebc84ae73c744d985706593
irieekup707 <irieekglass@gmail.com>
2019-05-01 12:07:43 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS415P4
+//
+//  Created by Ryan  Martino on 5/1/19.
+//  Copyright © 2019 Ryan  Martino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
