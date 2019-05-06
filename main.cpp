eb1d320990515fac0fbb3c456e6629d317b7bd34
irieekup707 <irieekglass@gmail.com>
2019-05-05 23:05:38 -0700

    Update main.cpp
    knapsack1a

--- main.cpp
+++ main.cpp
@@ -8,8 +8,51 @@
 
 #include <iostream>
 
+
+int knapsack(int weight[], int val[],int cap, int n);
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 4;
+    
+    int weight[] = {2,1,3,2};
+    
+    int val[] = {12, 10, 20, 15};
+    
+    int capacity = 5;
+    
+    int res = knapsack(weight, val, capacity, n);
+    
+    std::cout << "result is : " << res << std::endl;
+    
     return 0;
+}
+int knapsack(int weight[], int val[],int cap, int n)
+{
+     int i, j;
+    int k[n+1][cap+1];
+    
+    
+    for(i = 0; i <= n; i++)
+    {
+        k[i][0] = 0;
+    }
+    for(j = 0; j <= n; j++)
+    {
+        k[0][j] = 0;
+    }
+     
+    for(i=0; i<= n;i++)
+    {
+        for(j = 0; j <= cap; j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                k[i][j]= 0;
+            }else if(weight[i] <= j)
+        {
+            k[i][j] = std::max(k[i-1][j],val[i-1] + k[i-1][j-weight[i-1]]);
+        } else
+            k[i][j] = k[i-1][j];
+        }
+    }
+    return k[n][cap ];
 }
